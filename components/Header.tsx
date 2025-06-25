7ad9376c65857811c202ee78b2218ac3db370910
Dmitrii Antyushin <d.antyushin@tg.dunice.net>
2025-06-25 12:59:04 +0300

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,34 +1,43 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import styles from '../styles/Header.module.css';
 
 export default function Header() {
   return (
-    <header>
-      <div className={styles.logo}></div>
-      <div className={styles.contacts}>
-        <span className={styles.email}></span>
-        <span className={styles.phone}></span>
-      </div>
-      <nav className={styles.navList}>
-        <li>
-          <Link href={'/equipment'}>ОБОРУДОВАНИЕ</Link>
-        </li>
-        <li>
-          <Link href={'/application'}>ПРИМЕНЕНИЕ</Link>
-        </li>
-        <li>
-          <Link href={'/articles'}>СТАТЬИ</Link>
-        </li>
-        <li>
-          <Link href={'/specifications'}>ПОКАЗАНИЯ</Link>
-        </li>
-        <li>
-          <Link href={'/about'}>О КОМПАНИИ</Link>
-        </li>
-        <li>
-          <Link href={'/contacts'}>КОНТАКТЫ</Link>
-        </li>
-      </nav>
-    </header>
+    <>
+      <header className={styles.header}>
+        <div className={styles.logo}>
+          <Link href={'/'}>
+            <Image src={'/logo.svg'} alt={'logo'} width={45} height={25} />
+          </Link>
+        </div>
+        <div className={styles.contacts}>
+          <div className={styles.email}>everest-baro@mail.ru</div>
+          <div className={styles.phone}>+7 (911) 109-13-99</div>
+        </div>
+        <nav className={styles.navList}>
+          <li>
+            <Link href={'/equipment'}>ОБОРУДОВАНИЕ</Link>
+          </li>
+          <li>
+            <Link href={'/application'}>ПРИМЕНЕНИЕ</Link>
+          </li>
+          <li>
+            <Link href={'/articles'}>СТАТЬИ</Link>
+          </li>
+          <li>
+            <Link href={'/specifications'}>ПОКАЗАНИЯ</Link>
+          </li>
+          <li>
+            <Link href={'/about'}>О КОМПАНИИ</Link>
+          </li>
+          <li>
+            <Link href={'/contacts'}>КОНТАКТЫ</Link>
+          </li>
+        </nav>
+        <button className={styles.button}>ПОЛУЧИТЬ ПРЕЗЕНТАЦИЮ</button>
+      </header>
+      <div className={styles.row}></div>
+    </>
   );
 }
