39c39d151f5048b44cc792349c85f6abe610fcc2
Dmitrii Antyushin <d.antyushin@tg.dunice.net>
2025-07-11 11:25:52 +0300

    feat: add head to equipments

--- pages/equipment/index.tsx
+++ pages/equipment/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { Section } from '@radix-ui/themes';
 import BaroCard from '@/components/BaroCard';
 import { mockBarocams } from '@/data/barocams';
@@ -5,21 +6,28 @@ import styles from '@/styles/Equipment.module.css';
 
 export default function Equipment() {
   return (
-    <Section className={styles.equipment}>
-      <h1 className={styles.title}>Кислородные барокамеры Эверест</h1>
-      <div className={styles.cards}>
-        {mockBarocams.map(({ id, title, description, cost, img }) => (
-          <BaroCard
-            key={id}
-            id={id}
-            img={img}
-            alt={title}
-            title={title}
-            description={description}
-            cost={cost}
-          />
-        ))}
-      </div>
-    </Section>
+    <>
+      <Head>
+        <title>Оборудование</title>
+        <meta name="description" content="Барокамеры Эверест" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Section className={styles.equipment}>
+        <h1 className={styles.title}>Кислородные барокамеры Эверест</h1>
+        <div className={styles.cards}>
+          {mockBarocams.map(({ id, title, description, cost, img }) => (
+            <BaroCard
+              key={id}
+              id={id}
+              img={img}
+              alt={title}
+              title={title}
+              description={description}
+              cost={cost}
+            />
+          ))}
+        </div>
+      </Section>
+    </>
   );
 }
